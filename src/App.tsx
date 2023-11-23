524e752987d5d13f3a1435a41ab71bc5703d93ed
ycy <yvonnella.keks@gmail.com>
2023-11-24 00:18:19 +0100

    Rebrand, Bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -7,45 +7,59 @@ const APIKey = "AIzaSyDpkqVcuwpwkwD9KYM7ksPM9D06hcUkoIQ";
 const APIURL = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyDpkqVcuwpwkwD9KYM7ksPM9D06hcUkoIQ";
 
 const App = () => {
-  let savedFileName = "";
+  // let savedFiles: any = [];
+  const savedFiles: any = useState([]);
 
   const imageUpload = (e: any) => {
-    const file = e.target.files[0];
-    getBase64(file).then(base64 => {
-      localStorage["fileBase64"] = base64;
-      savedFileName = file.name;
-    });
+    try {
+      // var temp: any = [];
+      // if (savedFiles.length > 0) {
+      const fileList = Array.from(e.target.files);
+      fileList.forEach((file: any) => {
+        getBase64(file).then(base64 => {
+          savedFiles.push({ img: base64, name: file.name })
+        });
+      })
+      // setSavedFiles(savedFiles);
+      // }
+    } catch (error) {
+      console.error(error)
+    }
   };
 
   const downloadOutput = async () => {
-    let out = dataURLtoFile(localStorage["fileBase64"], savedFileName);
-    const url = window.URL.createObjectURL(out);
-    const link = document.createElement('a');
-    link.href = url;
-    let result = savedFileName;
-    let savedFile = localStorage["fileBase64"].split(',')[1];
-
-    const requestData = {
-      requests: [
-        {
-          image: {
-            content: savedFile
-            // source: {
-            //   imageUri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogospng.org%2Fdownload%2Fcoca-cola%2Flogo-coca-cola-4096.png&f=1&nofb=1&ipt=3fa7cec9f3780b920188c7abcbdb7d258478fc174f7c26c2f6103ace152993fa&ipo=images"
-            // }
-          },
-          features: [{
-            type: "LOGO_DETECTION", maxResults: 1
-          }]
-        }]
-    };
-    const apiResponse = await axios.post(APIURL, requestData);
-    result = apiResponse.data.responses[0].logoAnnotations[0].description;
-
-    link.download = "" + result; //filename
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    // if (savedFiles.length > 0) {
+    savedFiles.forEach(async (file: any) => {
+      try {
+        let out = dataURLtoFile(file.img, file.name);
+        const url = window.URL.createObjectURL(out);
+        const link = document.createElement('a');
+        link.href = url;
+        let result = file.name;
+        let savedFile = file.img.split(',')[1];
+        const requestData = {
+          requests: [
+            {
+              image: {
+                content: savedFile
+              },
+              features: [{
+                type: "LOGO_DETECTION", maxResults: 1
+              }]
+            }]
+        };
+        const apiResponse = await axios.post(APIURL, requestData);
+        result = apiResponse.data.responses[0].logoAnnotations[0].description;
+        link.download = "" + result; //filename
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      } catch (error) {
+        console.error(error)
+      }
+    })
+    // setSavedFiles([]);
+    // }
   }
 
 
@@ -53,17 +67,23 @@ const App = () => {
   return (
     <div className="App">
       <div className='spacer'></div>
-      <h1 className='glowtext'>Rename Images to Logoname AI</h1>
+      <div className="headersection">
+        <h1 className='glowtext head1'>AI Batch Rename</h1>
+        <h3 className='glowtext head3'>Images to Logoname</h3>
+      </div>
       <div className='spacer'></div>
-
-      <input
-        type="file"
-        id="imageFile"
-        name='imageFile'
-        onChange={imageUpload} />
-
       <div className='spacer'></div>
-      <button onClick={downloadOutput}>Download</button>
+      <div className='center'>
+        <input
+          type="file"
+          id="imageFile"
+          name='imageFile'
+          onChange={imageUpload}
+          multiple />
+        <div className='spacer'></div>
+        <button onClick={downloadOutput}>Convert</button>
+      </div>
+      <p className="credits">Made by Yvonne Creter, using Google Vision</p>
     </div>
   );
 }
@@ -88,8 +108,6 @@ function dataURLtoFile(dataurl: any, filename: string) {
   }
   return new File([u8arr], filename, { type: mime });
 }
-
-
 
 export default App;
 
